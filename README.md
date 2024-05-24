23756104747b2175f5541b0014172c54c5279357
lucasfcomaru <lukscomaru@gmail.com>
2024-05-24 16:15:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<img src="/assets/banner-viacep-api.png"/>
+<p align="center">
+    <img src="https://img.shields.io/github/repo-size/lucasfcomaru/consulta_viacep?style=for-the-badge" alt="Repo size" title="Repo size"/>
+    <img src="https://img.shields.io/github/languages/count/lucasfcomaru/consulta_viacep?style=for-the-badge" alt="Language count" title="Language count"/>
+    <img src="https://img.shields.io/github/forks/lucasfcomaru/consulta_viacep?style=for-the-badge" alt="Forks" title="Forks"/>
+    <img src="https://img.shields.io/bitbucket/issues/lucasfcomaru/consulta_viacep?style=for-the-badge" alt="Open issues" title="Open issues"/>
+    <img src="https://img.shields.io/bitbucket/pr-raw/lucasfcomaru/consulta_viacep?style=for-the-badge" alt="Open pull requests count" title="Open pull requests"/>
+    <img src="http://img.shields.io/static/v1?label=STATUS&message=CONCLUIDO&color=GREEN&style=for-the-badge"/>
+    <img src="http://img.shields.io/static/v1?label=License&message=MIT&color=green&style=for-the-badge"/>
+</p>
+
+# 📦 Consulta de endereço com API ViaCEP
+## 📢 Descrição do Projeto
+### 🎯 Objetivo
+<p align="left">
+    O objetivo deste projeto é exercitar o conhecimento acerca do consumo de APIs utilizando JavaScript, juntamente com HTML5 e CSS3 para criar o projeto.
+<p align="left">
+    <i>O projeto tem caráter experimental e visa a prática do conhecimento.</i>
+</p>
+
+### 🛠️ Ferramentas utilizadas
+<ul>
+    <li><b>JavaScript:</b> Linguagem de programação principal.</li>
+    <li><b>Visual Studio Code:</b> Ambiente de Desenvolvimento Integrado (IDE).</li>
+    <li><b>Bootstrap:</b> Framework utilizado para estilizar alguns elementos da página.</li>
+    <li><b>ViaCEP:</b> API utilizada para retornar o endereço pesquisado.</li>
+
+</ul>
+
+## 💻 Pré-requisitos
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- Você tem uma máquina `<Windows / Linux / Mac>` ou um smartphone `<Android / iOS>`.
+- Você tem a versão mais recente de um dos principais navegadores `<Chrome / Firefox / Edge / Opera>` instalada.
+
+## ☕ Usando o projeto para pesquisar endereços
+<ul>
+    <li>Etapa 1: abra o arquivo index.html.</li>
+    <li>Etapa 2: insira o número do CEP sem traços que deseja consultar.</li>
+    <li>Etapa 3: Aperte o botão para pesquisar.</li>
+</ul>
---
+++ poke_api.js
@@ -0,0 +1,97 @@
+function consulta_pokemon() {
+    // pegando o valor do nome do pokémon no html e tazendo para o javascript través de um seletor CSS
+    // e transformando em caracteres minúsculos
+    let pokemon = document.querySelector('#pokemon').value.toLowerCase();
+    // concatenando a variável para substituir pelo valor do cep dentro da string
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+    // pega os dados da API
+    fetch(url).then(function(response){
+        response.json().then(function(data){
+            //trocar pela função que vai mostrar os resultados
+            mostra_resultado(data);
+        })
+    })
+    
+}
+
+function mostra_resultado(dados) {
+    // pega a div pelo onde vai armazenar o resultado pelo ID
+    let resultado = document.querySelector('#resultado');
+    // pegando o valor do nome do pokémon no html e tazendo para o javascript través de um seletor CSS
+    // e transformando em caracteres minúsculos
+    let pokemon = document.querySelector('#pokemon').value.toLowerCase();
+    // pega a div pelo onde vai armazenar a imagem pelo ID
+    let imagem = document.querySelector('#imagem');
+    // pega a div pelo onde vai armazenar os atributos pelo ID
+    let atributos = document.querySelector('#atributos');
+
+    try {
+        // tratando caso não tenha nada digitado no input
+        if (pokemon == "") {
+            resultado.innerHTML = "<p>Por favor, digite o nome do pokémon.</p>";
+        } else {
+            if (dados.abilities.length == 1) {
+                // Pegando os atributos principais
+                resultado.innerHTML = ` <img src="${dados.sprites.front_default}" title="${dados.name}" alt="${dados.name}">
+                                        <p><b>Número: </b>${dados.id}</p>
+                                        <p><b>Nome: </b>${dados.name}</p>
+                                        <p><b>Tipo: </b>${dados.types[0].type.name}</p>
+                                        <!-- convertendo altura -->
+                                        <p><b>Altura: </b>${dados.height}0 cm</p>
+                                        <!-- convertendo peso -->
+                                        <p><b>Peso: </b>${dados.weight / 10} kg</p>
+                                        <p><b>Habilidades: </b>${dados.abilities[0].ability.name}`;
+                // Pegando os atributos
+                atributos.innerHTML = ` <p><b>HP: </b>${dados.stats[0].base_stat}</p>
+                                        <p><b>Ataque: </b>${dados.stats[1].base_stat}</p>
+                                        <p><b>Defesa: </b>${dados.stats[2].base_stat}</p>
+                                        <p><b>Ataque Especial: </b>${dados.stats[3].base_stat}</p>
+                                        <p><b>Defesa Especial: </b>${dados.stats[4].base_stat}</p>
+                                        <p><b>Velocidade: </b>${dados.stats[5].base_stat}</p>`;
+
+            } else if (dados.abilities.length == 2){
+                // Pegando os atributos principais
+                resultado.innerHTML = ` <img src="${dados.sprites.front_default}" title="${dados.name}" alt="${dados.name}">
+                                        <p><b>Número: </b>${dados.id}</p>
+                                        <p><b>Nome: </b>${dados.name}</p>
+                                        <p><b>Tipo: </b>${dados.types[0].type.name}</p>
+                                        <!-- convertendo altura -->
+                                        <p><b>Altura: </b>${dados.height}0 cm</p>
+                                        <!-- convertendo peso -->
+                                        <p><b>Peso: </b>${dados.weight / 10} kg</p>
+                                        <p><b>Habilidades: </b>${dados.abilities[0].ability.name}, ${dados.abilities[1].ability.name}</p>`;
+                // Pegando os atributos
+                atributos.innerHTML = ` <p><b>HP: </b>${dados.stats[0].base_stat}</p>
+                                        <p><b>Ataque: </b>${dados.stats[1].base_stat}</p>
+                                        <p><b>Defesa: </b>${dados.stats[2].base_stat}</p>
+                                        <p><b>Ataque Especial: </b>${dados.stats[3].base_stat}</p>
+                                        <p><b>Defesa Especial: </b>${dados.stats[4].base_stat}</p>
+                                        <p><b>Velocidade: </b>${dados.stats[5].base_stat}</p>`;
+            } else {
+                // Pegando os atributos principais
+                resultado.innerHTML = ` <img src="${dados.sprites.front_default}" title="${dados.name}" alt="${dados.name}">
+                                        <p><b>Número: </b>${dados.id}</p>
+                                        <p><b>Nome: </b>${dados.name}</p>
+                                        <p><b>Tipo: </b>${dados.types[0].type.name}</p>
+                                        <!-- convertendo altura -->
+                                        <p><b>Altura: </b>${dados.height}0 cm</p>
+                                        <!-- convertendo peso -->
+                                        <p><b>Peso: </b>${dados.weight / 10} kg</p>
+                                        <p><b>Principais habilidades: </b>${dados.abilities[0].ability.name}, ${dados.abilities[1].ability.name}, ${dados.abilities[2].ability.name}</p>`;
+                // Pegando os atributos
+                atributos.innerHTML = ` <p><b>HP: </b>${dados.stats[0].base_stat}</p>
+                                        <p><b>Ataque: </b>${dados.stats[1].base_stat}</p>
+                                        <p><b>Defesa: </b>${dados.stats[2].base_stat}</p>
+                                        <p><b>Ataque Especial: </b>${dados.stats[3].base_stat}</p>
+                                        <p><b>Defesa Especial: </b>${dados.stats[4].base_stat}</p>
+                                        <p><b>Velocidade: </b>${dados.stats[5].base_stat}</p>`;
+            }
+        }
+        
+    } catch(erro) {
+        // tratando caso dê um erro na busca
+        resultado.innerHTML = `<p>Pokémon não encontrado.</p>`
+
+    }
+}
